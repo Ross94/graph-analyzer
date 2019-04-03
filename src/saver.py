958273fddde4e8c423cfe7ff8582f7048babe872
Daniele Rosetti <daniele.rosetti@studio.unibo.it>
2019-04-03 16:48:55 +0200

    create folder for results

--- src/saver.py
+++ src/saver.py
@@ -1,6 +1,12 @@
 import json
+import os
 
 def save_json_file(data):
-    RESULTS_PATH = "../results.json"
-    with open(RESULTS_PATH, 'w') as outfile:
-        json.dump(data, outfile)
+	RESULT_DIRECTORY = "./../result"
+	RESULTS_PATH = RESULT_DIRECTORY + "/results.json"
+
+	if not os.path.exists(RESULT_DIRECTORY):
+		os.makedirs(RESULT_DIRECTORY)
+		
+	with open(RESULTS_PATH, 'w') as outfile:
+		json.dump(data, outfile)
