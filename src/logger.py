98a43ef0444aa6dd4c3dbc6981c0e7cce16ed3f7
Daniele Rosetti <daniele.rosetti@studio.unibo.it>
2019-04-05 11:42:14 +0200

    beautify log

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,7 @@ def log(data):
     LOG_DIRECTORY = "./../log"
     LOG_PATH = LOG_DIRECTORY + "/log.log"
 
-    log_str = "[" + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "]" + data
+    log_str = "[" + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "] " + data
 
     if not os.path.exists(LOG_DIRECTORY):
 		os.makedirs(LOG_DIRECTORY)
